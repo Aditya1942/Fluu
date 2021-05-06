778c8e332e9ec3794458746a5a2fcfa8a8cd7013
aditya parmar <parmaraditya1942@gmail.com>
2021-05-06 10:14:52 +0530

    login with facebook completed

--- screens/auth/SignInScreen.js
+++ screens/auth/SignInScreen.js
@@ -3,6 +3,7 @@ import {View, Text, Button} from 'react-native';
 import {setUserData} from './Storage';
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {LoginManager, AccessToken} from 'react-native-fbsdk';
 
 const SignInScreen = ({navigation}) => {
   async function onGoogleButtonPress() {
@@ -12,6 +13,32 @@ const SignInScreen = ({navigation}) => {
     const googleCredential = auth.GoogleAuthProvider.credential(idToken);
     // Sign-in the user with the credential
     return auth().signInWithCredential(googleCredential);
+  }
+  async function onFacebookButtonPress() {
+    // Attempt login with permissions
+    const result = await LoginManager.logInWithPermissions([
+      'public_profile',
+      'email',
+    ]);
+
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+
+    // Once signed in, get the users AccesToken
+    const data = await AccessToken.getCurrentAccessToken();
+
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+
+    // Create a Firebase credential with the AccessToken
+    const facebookCredential = auth.FacebookAuthProvider.credential(
+      data.accessToken,
+    );
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(facebookCredential);
   }
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -47,7 +74,18 @@ const SignInScreen = ({navigation}) => {
           }
           title="SignIn Using Google"
         />
-        <Button onPress={() => {}} title="SignIn Using Facebook" />
+        <Button
+          onPress={() =>
+            onFacebookButtonPress().then(data => {
+              console.log('Signed in with Facebook!', data);
+              setUserData(data);
+              navigation.reset({
+                routes: [{name: 'HomeScreen'}],
+              });
+            })
+          }
+          title="SignIn Using Facebook"
+        />
       </View>
     </View>
   );
--- screens/home/ProfileScreen.js
+++ screens/home/ProfileScreen.js
@@ -2,12 +2,48 @@ import React from 'react';
 import {View, Text, Button} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
-
+import {
+  AccessToken,
+  LoginManager,
+  GraphRequest,
+  GraphRequestManager,
+} from 'react-native-fbsdk';
 const ProfileScreen = ({navigation}) => {
   const signOut = async () => {
+    var current_access_token = '';
     try {
       await GoogleSignin.revokeAccess();
       await GoogleSignin.signOut();
+    } catch (error) {
+      console.error(error);
+    }
+    try {
+      AccessToken.getCurrentAccessToken()
+        .then(data => {
+          current_access_token = data.accessToken.toString();
+        })
+        .then(() => {
+          let logout = new GraphRequest(
+            'me/permissions/',
+            {
+              accessToken: current_access_token,
+              httpMethod: 'DELETE',
+            },
+            (error, result) => {
+              if (error) {
+                console.error('Error fetching data: ' + error.toString());
+              } else {
+                LoginManager.logOut();
+              }
+            },
+          );
+          new GraphRequestManager().addRequest(logout).start();
+        })
+
+        .catch(error => {
+          console.error(error);
+          navigation.navigate('Login');
+        });
     } catch (error) {
       console.error(error);
     }
